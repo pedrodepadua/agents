a9892e349f75717da4aa94ae6abac98f43d2bef5
pedrodepadua <pedro3ero@gmail.com>
2025-07-01 13:17:27 +0200

    FIRST PROJECT COMPLETED-still need to add gemini

---
+++ 1_foundations/README.md
@@ -0,0 +1,6 @@
+---
+title: Pedro_de_Padua_Career_Profile
+app_file: app.py
+sdk: gradio
+sdk_version: 5.34.2
+---
--- 1_foundations/app.py
+++ 1_foundations/app.py
@@ -77,7 +77,7 @@ class Me:
 
     def __init__(self):
         self.openai = OpenAI()
-        self.name = "Ed Donner"
+        self.name = "Pedro de Padua Escobar"
         reader = PdfReader("me/linkedin.pdf")
         self.linkedin = ""
         for page in reader.pages:
@@ -106,10 +106,12 @@ Your responsibility is to represent {self.name} for interactions on the website 
 You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
 Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
 If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
-If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
+If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their name and email and record it using your record_user_details tool. \
+If the user is asking something that's not about my career or professional background then record it using your record_unknown_question tool. \
+If the user ask a common HR question (for example, the biggest challenges you've faced at work) and you do not have the information in {self.summary} or {self.linkedin} then record it using your record_unknown_question tool. "
 
         system_prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
-        system_prompt += f"With this context, please chat with the user, always staying in character as {self.name}."
+        system_prompt += f"With this context, please chat with the user, always staying in character as {self.name} and speak as if you were {self.name}."
         return system_prompt
     
     def chat(self, message, history):
